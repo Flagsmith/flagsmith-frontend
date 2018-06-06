aff969732639d0cb325f39966de96d1b2ce4506a
Luke Fanning <luke@solidstategroup.com>
2018-06-06 16:44:53 +0100

    Few fixes. Finishing off invite testing. Setting up e2e teardown

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,6 +8,7 @@ var server;
 
 const Project = require('../common/project');
 const fetch = require('node-fetch');
+global.testHelpers = require('./helpers');
 
 module.exports = Object.assign(
   {
@@ -17,16 +18,41 @@ module.exports = Object.assign(
     },
     after: (browser, done) => {
       server.kill('SIGINT');
-      // fetch(`${Project.api}tests`, {
-      //   method: 'DELETE',
-      // })
-      //   .then(res => {
-      //     done();
-      //   })
-      done();
+      var token;
+      if (process.env['E2E_TEST_TOKEN']) {
+        token = process.env['E2E_TEST_TOKEN'];
+      } else {
+        const fs = require('fs');
+        if (fs.existsSync('./tests/tokens.json')) {
+          token = require('./tokens.json')[Project.env];
+        } else {
+          console.log('No tokens.json found for teardown. Either set E2E_TEST_TOKEN in your environment or create the file in /tests');
+        }
+      }
+      if (token) {
+        fetch(`${Project.api}e2etests/teardown/?format=json`, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'X-E2E-Test-Auth-Token': token
+            },
+            body: JSON.stringify({})
+        }).then(res => {
+          if (res.ok) {
+            console.log('\n', '\x1b[32m', 'e2e teardown successful', '\x1b[0m', '\n');
+            done();
+          } else {
+            console.error('\n', '\x1b[31m', 'e2e teardown failed', res.status, '\x1b[0m', '\n');
+          }
+        });
+      } else {
+        console.error('\n', '\x1b[31m', 'e2e teardown failed - no available token', '\x1b[0m', '\n');
+      }
     }
   },
   require('./main.test'), // Main flow tests
+  require('./invite.test'), // Invite user tests
   require('./register-fail.test'), // Registration failure tests
   require('./login-fail.test') // Login failure tests
 );
