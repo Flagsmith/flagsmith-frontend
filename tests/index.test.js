66ab461e4e33a1a28131c66db8599f4c35fdd038
kyle <kyle@solidstategroup.com>
2018-06-11 10:05:22 +0100

    Improve e2e test

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,50 +9,55 @@ var server;
 const Project = require('../common/project');
 const fetch = require('node-fetch');
 global.testHelpers = require('./helpers');
-
-module.exports = Object.assign(
-  {
-    before: (browser, done) => {
-      server = fork('./server');
-      server.on('message', () => done());
-    },
-    after: (browser, done) => {
-      server.kill('SIGINT');
-      var token;
-      if (process.env['E2E_TEST_TOKEN']) {
+const clearDown = function(browser,done) {
+    var token;
+    if (process.env['E2E_TEST_TOKEN']) {
         token = process.env['E2E_TEST_TOKEN'];
-      } else {
+    } else {
         const fs = require('fs');
         if (fs.existsSync('./tests/tokens.json')) {
-          token = require('./tokens.json')[Project.env];
+            token = require('./tokens.json')[Project.env];
         } else {
-          console.log('No tokens.json found for teardown. Either set E2E_TEST_TOKEN in your environment or create the file in /tests');
+            console.log('No tokens.json found for teardown. Either set E2E_TEST_TOKEN in your environment or create the file in /tests');
         }
-      }
-      if (token) {
+    }
+    if (token) {
         fetch(`${Project.api}e2etests/teardown/?format=json`, {
             method: 'POST',
             headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'X-E2E-Test-Auth-Token': token
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-E2E-Test-Auth-Token': token
             },
             body: JSON.stringify({})
         }).then(res => {
-          if (res.ok) {
-            console.log('\n', '\x1b[32m', 'e2e teardown successful', '\x1b[0m', '\n');
-            done();
-          } else {
-            console.error('\n', '\x1b[31m', 'e2e teardown failed', res.status, '\x1b[0m', '\n');
-          }
+            if (res.ok) {
+                console.log('\n', '\x1b[32m', 'e2e teardown successful', '\x1b[0m', '\n');
+                done();
+            } else {
+                console.error('\n', '\x1b[31m', 'e2e teardown failed', res.status, '\x1b[0m', '\n');
+            }
         });
-      } else {
+    } else {
         console.error('\n', '\x1b[31m', 'e2e teardown failed - no available token', '\x1b[0m', '\n');
-      }
     }
-  },
-  require('./main.test'), // Main flow tests
-  require('./invite.test'), // Invite user tests
-  require('./register-fail.test'), // Registration failure tests
-  require('./login-fail.test') // Login failure tests
+};
+
+module.exports = Object.assign(
+    {
+        before: (browser, done) => {
+            server = fork('./server');
+            server.on('message', () => {
+                clearDown(browser,done);
+            });
+        },
+        after: (browser, done) => {
+            server.kill('SIGINT');
+            clearDown(browser,done);
+        }
+    },
+    require('./main.test'), // Main flow tests
+    require('./invite.test'), // Invite user tests
+    require('./register-fail.test'), // Registration failure tests
+    require('./login-fail.test') // Login failure tests
 );
