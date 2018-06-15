50096c3bac98def4a480982faddcb342d55ccca6
kyle <kyle@solidstategroup.com>
2018-06-15 14:40:29 +0100

    What are feature flags copy

--- web/components/pages/WhatAreFeatureFlagsPage.js
+++ web/components/pages/WhatAreFeatureFlagsPage.js
@@ -15,18 +15,26 @@ const TheComponent = class extends Component {
                     <div className={"container"}>
                         <h3>What are feature flags?</h3>
                         <div className={"row"}>
-                            <div className={"col-md-6"}>
-                                A Feature Flag is a technique to turn some functionality of your application off, via
-                                configuration, without deploying new code. If you’re employing CI/CD and releasing often
-                                they’re pretty important.
+                            <div className={"col-xl-6"}>
+                                <p>
+                                    A Feature Flag is a technique to turn some functionality of your application off,
+                                    via configuration, without deploying new code. If you’re employing CI/CD and
+                                    releasing
+                                    often they’re pretty important.
+                                </p>
+                                <p>
+                                    With bullet train feature flags can be turned on and off per environment, also you
+                                    can enable flags for specific users. For example, this could be used to show
+                                    features to your internal team in production or for beta testers.
+                                </p>
                             </div>
-                            <div className={"col-md-6"}>
+                            <div className={"col-xl-6"}>
                                 <p className={"faint text-center"}>
                                     EXAMPLE: our interactive try it panels throughout the site are controlled by feature
                                     flags.
                                 </p>
                                 <div className={"text-center"}>
-                                    <Gif height={500} pausedSrc={"/images/feature-flag.png"}
+                                    <Gif width={"100%"} pausedSrc={"/images/feature-flag.png"}
                                          src={"/images/feature-flag.gif"}/>
                                 </div>
                             </div>
@@ -37,20 +45,31 @@ const TheComponent = class extends Component {
                     <div className={"container"}>
                         <h3>What is remote config?</h3>
                         <div className={"row"}>
-                            <div className={"col-md-6"}>
-                                It's great being able to turn features on and off without doing a build, but with some
-                                features,
-                                it's not as simple as that. This is where remote config comes in.
-                                Often there's a need to configure feature properties, for example updating some text to
-                                promote an
-                                in-app currency sale or even the copy on our homepage.
+                            <div className={"col-xl-6"}>
+                                <p>
+                                    It's great being able to turn features on and off without doing a build, but with
+                                    some
+                                    features,
+                                    it's not as simple as that. This is where remote config comes in.
+                                    Often there's a need to configure feature properties, for example updating some text
+                                    to
+                                    promote an
+                                    in-app currency sale or even the copy on our homepage.
+                                </p>
+                                <p>
+                                    Similarly with feature flags, Bullet Train lets you configure these per environment
+                                    and for specific users. With our upcoming user segments feature, this could be used
+                                    to show different messaging or environment variables to power user groups,
+                                    mobile/browser specific users and
+                                    much more.
+                                </p>
                             </div>
-                            <div className={"col-md-6"}>
+                            <div className={"col-xl-6"}>
                                 <p className={"faint text-center"}>
                                     EXAMPLE: our homepage sections are controlled with remote config
                                 </p>
                                 <div className={"text-center"}>
-                                    <Gif height={500} pausedSrc={"/images/remote-config.png"}
+                                    <Gif width={"100%"} pausedSrc={"/images/remote-config.png"}
                                          src={"/images/remote-config.gif"}/>
                                 </div>
                             </div>
@@ -60,6 +79,12 @@ const TheComponent = class extends Component {
                 <FormGroup className={"striped-section"}>
                     <div className={"container"}>
                         <h3>What benefits does this bring?</h3>
+
+                        <p>
+                            People who are not used to continuous integration might be wondering: why didn't I just
+                            release the feature the way I normally would? Some of the reasons for feature flags being
+                            such a good approach:
+                        </p>
                         <ul>
                             <li>
                                 It introduces the ability to test code in production with gradual/targeted rollouts to
