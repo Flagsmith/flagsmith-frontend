b9fa89e941da9ffc707a2873c222b87690ba0afe
andrewmoffatt <andrew@solidstategroup.com>
2018-09-05 17:11:49 +0100

    pricing page styling

--- web/components/pages/PricingPage.js
+++ web/components/pages/PricingPage.js
@@ -24,57 +24,96 @@ const HomePage = class extends React.Component {
             <div>
                 <div className="pricing">
                     <div className="container">
-                        <h2>Start using Bullet Train for free, then scale-up over time.</h2>
+                        <h2 className="text-center margin-bottom">Start using Bullet Train for free</h2>
+                        <p className="text-center">Then increase your your plan as your business grows.</p>
                         <div className={"col-md-12 row"}>
-                            <div className={"col-md-3"}>
-                                <Panel title={<h4 className="pricing-type">Free</h4>}>
-                                    <h5 className="pricing-price">$Free</h5>
-                                    <ul className="pricing-features">
-                                        <li><p>Up to 2,000 Monthly Active Users</p></li>
-                                        <li><p>Unlimited Administrator Accounts</p></li>
-                                        <li><p>Unlimited Projects</p></li>
-                                        <li><p>Unlimited Environments</p></li>
-                                        <li><p>Unlimited Feature Flags</p></li>
-                                    </ul>
-                                    <a href="#">Sign Up</a>
-                                </Panel>
+                            <div className={"col-md-3 pricing-panel"}>
+                                <div className="panel panel-default">
+                                    <div className="panel-content">
+                                        <p className="featured"> </p>
+                                        <p className="pricing-price">Free</p>
+                                        <img src="/images/growth.svg" alt="free icon" className="pricing-icon"/>
+                                        <p className="pricing-type">Free</p>
+                                        <p className="text-small text-center">30 day trial</p>
+                                        <a href="#" className="pricing-cta blue">Sign Up</a>
+                                    </div>
+                                    <div className="panel-footer">
+                                        <p className="text-small text-center link-style">What's included</p>
+                                        <ul className="pricing-features">
+                                            <li><p>Up to 2,000 Monthly Active Users</p></li>
+                                            <li><p>Unlimited Administrator Accounts</p></li>
+                                            <li><p>Unlimited Projects</p></li>
+                                            <li><p>Unlimited Environments</p></li>
+                                            <li><p>Unlimited Feature Flags</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={"col-md-3"}>
-                                <Panel title={<h4 className="pricing-type">Start-Up</h4>}>
-                                    <h5 className="pricing-price">$29 / month</h5>
-                                    <ul className="pricing-features">
-                                        <li><p>Up to 10,000 Monthly Active Users</p></li>
-                                        <li><p>Unlimited Administrator Accounts</p></li>
-                                        <li><p>Unlimited Projects</p></li>
-                                        <li><p>Unlimited Environments</p></li>
-                                        <li><p>Unlimited Feature Flags</p></li>
-                                        <li><p>Email Technical Support</p></li>
-                                    </ul>
-                                    <a href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="startup">Subscribe</a>
-                                </Panel>
+                            <div className={"col-md-3 pricing-panel"}>
+                                <div className="panel panel-default">
+                                    <div className="panel-content">
+                                        <p className="featured"> </p>
+                                        <p className="pricing-price">Start-Up</p>
+                                        <img src="/images/startup.svg" alt="Startup icon" className="pricing-icon"/>
+                                        <p className="pricing-type">$29</p>
+                                        <p className="text-small text-center">billed monthly</p>
+                                        <a href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="startup" className="pricing-cta blue">Subscribe</a>
+                                    </div>
+                                    <div className="panel-footer">
+                                        <p className="text-small text-center link-style">What's included</p>
+                                        <ul className="pricing-features">
+                                            <li><p>Up to 10,000 Monthly Active Users</p></li>
+                                            <li><p>Unlimited Administrator Accounts</p></li>
+                                            <li><p>Unlimited Projects</p></li>
+                                            <li><p>Unlimited Environments</p></li>
+                                            <li><p>Unlimited Feature Flags</p></li>
+                                            <li><p>Email Technical Support</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={"col-md-3"}>
-                                <Panel title={<h4 className="pricing-type">Scale-Up</h4>}>
-                                    <h5 className="pricing-price">$99 / month</h5>
-                                    <ul className="pricing-features">
-                                        <li><p>Up to 50,000 Monthly Active Users</p></li>
-                                        <li><p>All Startup Features</p></li>
-                                        <li><p>Telephone Technical Support</p></li>
-                                    </ul>
-                                    <a href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="scale-up">Subscribe</a>
-                                </Panel>
+                            <div className={"col-md-3 pricing-panel"}>
+                                <div className="panel panel-default">
+                                    <div className="panel-content">
+                                        <p className="featured">Most Popular</p>
+                                        <p className="pricing-price">Scale-Up</p>
+                                        <img src="/images/layers.svg" alt="Scale-up icon" className="pricing-icon"/>
+                                        <p className="pricing-type">$99</p>
+                                        <p className="text-small text-center">billed monthly</p>
+                                        <a href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="scale-up" className="pricing-cta">Subscribe</a>
+                                    </div>
+                                    <div className="panel-footer">
+                                        <p className="text-small text-center link-style">What's included</p>
+                                        <ul className="pricing-features">
+                                            <li><p>Up to 50,000 Monthly Active Users</p></li>
+                                            <li><p>All Startup Features</p></li>
+                                            <li><p>Telephone Technical Support</p></li>
+                                        </ul>
+
+                                    </div>
+                                </div>
                             </div>
-                            <div className={"col-md-3"}>
-                                <Panel title={<h4 className="pricing-type">Enterprise</h4>}>
-                                    <h5 className="pricing-price">$Contact Us</h5>
-                                    <ul className="pricing-features">
-                                        <li><p>Over 50,000 Monthly Active Users</p></li>
-                                        <li><p>All Startup Features</p></li>
-                                        <li><p>Telephone Technical Support</p></li>
-                                        <li><p>Optional On Premise Installation</p></li>
-                                    </ul>
-                                    <a href="mailto:enterprise@bullettrain.io">Contact Us</a>
-                                </Panel>
+                            <div className={"col-md-3 pricing-panel"}>
+                                <div className="panel panel-default">
+                                    <div className="panel-content">
+                                        <p className="featured"> </p>
+                                        <p className="pricing-price">Enterprise</p>
+                                        <img src="/images/cubes.svg" alt="Enterprise icon" className="pricing-icon"/>
+                                        <p className="pricing-type">Contact Us</p>
+                                        <p className="text-small text-center">for enterprise pricing</p>
+                                        <a href="mailto:enterprise@bullettrain.io" className="pricing-cta blue">Contact Us</a>
+                                    </div>
+
+                                    <div className="panel-footer">
+                                        <p className="text-small text-center link-style">What's included</p>
+                                        <ul className="pricing-features">
+                                            <li><p>Over 50,000 Monthly Active Users</p></li>
+                                            <li><p>All Startup Features</p></li>
+                                            <li><p>Telephone Technical Support</p></li>
+                                            <li><p>Optional On Premise Installation</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
