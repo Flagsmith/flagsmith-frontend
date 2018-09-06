e6764b538dbd80b6c48d4f89d54dffd611ef2348
Luke Fanning <luke@solidstategroup.com>
2018-09-06 14:22:01 +0100

    SLA page. Legal nav. Routes changed

---
+++ web/components/LegalAside.js
@@ -0,0 +1,51 @@
+import React, {Component, PropTypes} from 'react';
+
+const TheComponent = class extends Component {
+	displayName: 'TheComponent'
+
+	static contextTypes = {
+		router: React.PropTypes.object.isRequired
+	};
+
+	constructor(props, context) {
+		super(props, context);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<Flex className={"aside " + (this.props.className||"")}>
+				<div className="brand-container text-center">
+					<Link to="/">
+						<Row>
+							<img height={34} src={"/images/icon-2.png"}/>
+							Bullet Train
+						</Row>
+					</Link>
+				</div>
+				<Flex>
+					<Flex className="links">
+						<h3 className="link-header">Agreeements</h3>
+						<Link
+							activeClassName={"active"}
+							to={`/legal/tos`
+							}>Terms of Service</Link>
+						<Link
+							activeClassName={"active"}
+							to={`/legal/sla`
+							}>Service Level Agreement</Link>
+						<h3 className="link-header">Policies</h3>
+						<Link
+							activeClassName={"active"}
+							to={`/legal/privacy-policy`
+							}>Privacy Policy</Link>
+					</Flex>
+				</Flex>
+			</Flex>
+		);
+	}
+};
+
+TheComponent.propTypes = {};
+
+module.exports = TheComponent;
