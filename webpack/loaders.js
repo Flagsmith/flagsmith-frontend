3885c8a28e7f63faee902509586c8efb9d3b22b4
kyle <kyle@solidstategroup.com>
2018-11-27 15:58:37 +0000

    fix md support

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -4,17 +4,17 @@ module.exports = [
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
     },
-    // { test: /.json$/, loader: "json-loader" },
+    { test: /.json$/, loader: "json-loader", exclude: /node_modules/},
     {
         test: /\.html$/,
         use: 'html-loader?attrs[]=source:src&attrs[]=img:src'
     },
     {
-        test: /\.md$/,
+        test: /\.(md|txt)$/,
         use: 'raw-loader'
     },
     {
-        test: /\.(md|otf|ttf|eot|png|jpg|jpeg|gif|svg|woff|woff2|ogv|mp4|webm)$/,
+        test: /\.(otf|ttf|eot|png|jpg|jpeg|gif|svg|woff|woff2|ogv|mp4|webm)$/,
         use: 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]'
     }
 ];
