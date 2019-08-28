d6ec984c9789e34a1584775e4bf55b9b4d54bc1d
Ben Rometsch <ben@solidstategroup.com>
2019-08-28 14:04:49 +0100

    Converted from staging

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,59 +1,80 @@
 // webpack.config.dev.js
+// Watches + deploys files minified + cachebusted
+
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    mode: 'development',
-    entry: [
-        'webpack-hot-middleware/client?reload=false',
-        './web/main.js',
-    ],
-    devServer: {
-        outputPath: __dirname,
+    devtool: 'source-map',
+    mode: 'production',
+
+    entry: {
+        main: './web/main.js',
     },
-    output: {
-        path: path.join(__dirname, '../build'),
-        filename: '[name].js',
-        publicPath: '/',
+    optimization: { // chunk bundle into Libraries, App JS and dumb components
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true, // set to true if you want JS source maps
+                extractComments: true,
+                uglifyOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                },
+            }),
+        ],
     },
     externals: {
-    // require('jquery') is external and available
-    //  on the global var jQuery
+        // require('jquery') is external and available
+        //  on the global var jQuery
         'jquery': 'jQuery',
     },
-    plugins: require('./plugins').concat([
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            __DEV__: true,
-        }),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            jquery: 'jquery',
-        }),
-    ]).concat(require('./pages').map((page) => {
-        console.log(page);
-        return new HtmlWebpackPlugin({
-            filename: `${page}.html`, // output
-            template: `./web/${page}.html`, // template to use
-        });
-    })),
+    output: {
+        path: path.join(__dirname, '../build/static'),
+        filename: '[name].[hash].js',
+        publicPath: 'https://dev.bullet-train.io/static/'
+    },
+
+    plugins: require('./plugins')
+        .concat([
+            // Clear out build folder
+            new CleanWebpackPlugin(['build'], { root: path.join(__dirname, '../') }),
+
+            new webpack.DefinePlugin({
+                __DEV__: false,
+            }),
+
+            // reduce filesize
+            new webpack.optimize.OccurrenceOrderPlugin(),
+
+            // pull inline styles into cachebusted file
+            new ExtractTextPlugin({ filename: 'style.[hash].css', allChunks: true }),
+
+        ]).concat(require('./pages').map((page) => {
+            console.log(page);
+            return new HtmlWebpackPlugin({
+                    filename: page + '.handlebars', //output
+                    template: './web/' + page + '.handlebars', //template to use
+                    "assets": { //add these script/link tags
+                        "client": "/[hash].js",
+                        "style": "style.[hash].css"
+                    }
+                }
+            )
+        })),
+
     module: {
-        rules: require('./loaders')
-            .concat([
-                {
-                    test: /\.scss$/,
-                    use: [{
-                        loader: 'style-loader', // creates style nodes from JS strings
-                    }, {
-                        loader: 'css-loader', // translates CSS into CommonJS
-                    }, {
-                        loader: 'sass-loader', // compiles Sass to CSS
-                    }],
-                },
-            ]),
+        rules: require('./loaders').concat([
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+            },
+        ]),
     },
 };
