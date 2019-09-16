99734e8cd56b879c0de7fc71f8edacb186a91cf5
niall <niall@solidstategroup.com>
2019-09-16 15:43:58 +0100

    Add links to documentation on features page

--- web/components/pages/HomePage.js
+++ web/components/pages/HomePage.js
@@ -279,7 +279,7 @@ Now you can manage feature flags and remote config across web, mobile and server
                                               }}
                                             >
                                                 <div className="form-intro text-center">
-                                                    <h3>User login</h3>
+                                                    <h3 className="mb-4">User login</h3>
                                                 </div>
                                                 {isInvite &&
                                                 <div className="notification flex-row">
