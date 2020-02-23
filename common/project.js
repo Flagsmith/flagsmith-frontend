314fb99100a44730bc65ec7732420ccc88efef82
kyle <kyle@solidstategroup.com>
2020-02-23 23:01:06 +0000

    Adjust dev project.js

--- common/project.js
+++ common/project.js
@@ -1,9 +1,9 @@
 module.exports = global.Project = {
     debug: false,
-    env: 'staging', // This is used for Sentry tracking
-    bulletTrain: 'ENktaJnfLVbLifybz34JmX', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
-    api: 'https://api-staging.bullet-train.io/api/v1/',
-    ga: 'UA-120237963-7', // This is our Google Analytics key
+    env: 'dev', // This is used for Sentry tracking
+    bulletTrain: '8KzETdDeMY7xkqkSkY3Gsg', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
+    api: 'https://api-dev.bullet-train.io/api/v1/',
+    ga: 'UA-120237963-3', // This is our Google Analytics key
     demoAccount: {
         email: 'kyle+bullet-train@solidstategroup.com',
         password: 'demo_account',
