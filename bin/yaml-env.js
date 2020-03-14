d807fb192b1fe18a674cb3c2c353e7715bfd19cd
kyle <kyle@solidstategroup.com>
2020-03-14 11:28:10 +0000

    Test node -> yaml env

--- bin/yaml-env.js
+++ bin/yaml-env.js
@@ -3,13 +3,15 @@
  */
 require('colors');
 const fs = require('fs');
+const extra = require('fs-extra');
 const path = require('path');
 
-const src = path.resolve(__dirname, '../env.yaml');
-
-const str = `
-SLACK_TOKEN: ${process.env.SLACK_TOKEN}
-EVENTS_SLACK_CHANNEL: ${process.env.EVENTS_SLACK_CHANNEL}
+const src = path.resolve(__dirname, '../app.yaml');
+let str = extra.readFileSync(src, 'utf8');
+str += `
+env_variables:
+  SLACK_TOKEN: ${process.env.SLACK_TOKEN}
+  EVENTS_SLACK_CHANNEL: ${process.env.EVENTS_SLACK_CHANNEL}
 `;
 
 fs.writeFileSync(src, str);
