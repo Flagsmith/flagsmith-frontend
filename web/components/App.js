a268e90f96f1172122e62c299f290e792d5756d0
Yiftah Barouch <yiftah@myobligo.com>
2020-06-03 16:01:41 +0300

    Added sorting to features page

--- web/components/App.js
+++ web/components/App.js
@@ -73,7 +73,7 @@ const App = class extends Component {
         }
 
         // Redirect on login
-        if (this.props.location.pathname == '/' || this.props.location.pathname.includes("/oauth") || this.props.location.pathname == '/login' || this.props.location.pathname == '/demo' || this.props.location.pathname == '/signup') {
+        if (this.props.location.pathname == '/' || this.props.location.pathname.includes('/oauth') || this.props.location.pathname == '/login' || this.props.location.pathname == '/demo' || this.props.location.pathname == '/signup') {
             if (redirect) {
                 this.context.router.history.replace(redirect);
             } else {
@@ -209,6 +209,7 @@ Click here to Sign
                                                     <div className="flex-column org-nav">
                                                         <Popover
                                                           className="popover-right"
+                                                          contentClassName="popover-bt"
                                                           renderTitle={toggle => (
                                                               <a id="org-menu" onClick={toggle}>
                                                                   {organisation ? organisation.name : ''}
