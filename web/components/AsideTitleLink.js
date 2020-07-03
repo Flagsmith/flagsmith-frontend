bde7d144b3a6ef024fc28f794427c0779b943750
niall <niall@solidstategroup.com>
2020-07-03 11:09:46 +0100

    environments tooltip

--- web/components/AsideTitleLink.js
+++ web/components/AsideTitleLink.js
@@ -8,17 +8,20 @@ const AsideTitleLink = class extends PureComponent {
 
     render() {
         return (
-            <Button id={this.props.id} className="btn--neutral no-pointer" to={this.props.to}>
-                <div className="flex-row space aside__title-wrapper">
-                    <span className="aside__link-text">{this.props.title}</span>
-                    <Tooltip
-                      title={<span className={`aside__link-icon ${this.props.iconClassName || ''}`} />}
-                      place="top"
-                    >
-                        {this.props.tooltip}
-                    </Tooltip>
-                </div>
-            </Button>
+            <Tooltip
+                title={
+                    <div id={this.props.id} className="flex-row space aside__title-wrapper">
+                        <span className="aside__link-text">{this.props.title}</span>
+                        <span className={`aside__link-icon ${this.props.iconClassName || ''}`} />
+
+
+
+                    </div>
+                }
+                place="top"
+            >
+                {this.props.tooltip}
+            </Tooltip>
         );
     }
 };
