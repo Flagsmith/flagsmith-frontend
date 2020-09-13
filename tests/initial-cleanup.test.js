1a0413b8256564a7bef54e20862211e48b2572b7
kyle <kyle@solidstategroup.com>
2020-09-13 15:37:38 +0100

    re-add delete organisation to e2e

--- tests/initial-cleanup.test.js
+++ tests/initial-cleanup.test.js
@@ -1,14 +1,14 @@
 /* eslint-disable func-names */
 module.exports = {
     '[Initial Cleanup Tests] - Delete Bullet Train Ltd organisation': function (browser) {
-        // browser
-        //     .click('#organisation-settings-link')
-        //     .waitForElementVisible('#delete-org-btn')
-        //     .click('#delete-org-btn')
-        //     .waitForElementVisible('[name="confirm-org-name"]')
-        //     .setValue('[name="confirm-org-name"]', 'Bullet Train Ltd')
-        //     .click('#confirm-del-org-btn')
-        //     .waitForElementVisible('#create-org-page');
+        browser
+            .click('#organisation-settings-link')
+            .waitForElementVisible('#delete-org-btn')
+            .click('#delete-org-btn')
+            .waitForElementVisible('[name="confirm-org-name"]')
+            .setValue('[name="confirm-org-name"]', 'Bullet Train Ltd')
+            .click('#confirm-del-org-btn')
+            .waitForElementVisible('#create-org-page');
         browser.end();
     },
 };
