3dd7b89c8b2e038109d1b1d657891c269ffa748a
kyle <kyle@solidstategroup.com>
2021-01-27 19:24:49 +0000

    segment override delete hit area

--- common/project.js
+++ common/project.js
@@ -1,10 +1,11 @@
 module.exports = global.Project = {
-    api: 'https://api-staging.flagsmith.com/api/v1/',
+    api: 'https://api-dev.flagsmith.com/api/v1/',
     flagsmithClientAPI: 'https://api.flagsmith.com/api/v1/',
-    flagsmith: 'ENktaJnfLVbLifybz34JmX', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
-    env: 'staging', // This is used for Sentry tracking
-    maintenance: false, // trigger maintenance mode
+    flagsmith: '8KzETdDeMY7xkqkSkY3Gsg', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
+    debug: false,
     delighted: true, // determines whether to shw delighted feedback widget
+    env: 'dev', // This is used for Sentry tracking
+    maintenance: false, // trigger maintenance mode
     demoAccount: {
         email: 'kyle+bullet-train@solidstategroup.com',
         password: 'demo_account',
